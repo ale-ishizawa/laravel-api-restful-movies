944342042b5084f09da39a754b37be6d64eb2320
Alessandro Navarro Ishizawa <a.navarro.ishizawa@gmail.com>
2022-11-29 19:26:09 -0300

    create base controller

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class BaseController extends Controller
+{
+
+    /**
+     *
+     * @return Response
+     */
+    public function sendResponse($result, $message)
+    {
+        $response = [
+            'success' => true,
+            'data' => $result,
+            'message' => $message
+        ];
+
+        return response()->json($response, 200);
+    }
+
+
+    /**
+     * @return Response
+     */
+    public function sendError($error, $errorMessages = [], $code = 404)
+    {
+        $response = [
+            'success' => false,
+            'message' => $error
+        ];
+
+        if (!empty($errorMessages)) {
+            $response['data'] = $errorMessages;
+        }
+
+        return response()->json($response, $code);
+    }
+}
