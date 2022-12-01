1421037d3cc8073325ce02e4f763c26e8fdae761
Alessandro Navarro Ishizawa <a.navarro.ishizawa@gmail.com>
2022-11-30 22:57:10 -0300

    feat: api tag crud

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\TagResource;
+use App\Models\Tag;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class TagController extends BaseController
+{
+
+    public function index()
+    {
+        return Tag::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'name' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Name is required.', $validator->errors());
+        }
+
+        $tag = Tag::create($input);
+
+        return $this->sendResponse(new TagResource($tag), 'Tag created successfully.');
+    }
+
+}
