107a09a852b0b7a1c588496425603f7cc018a6d8
Alessandro Navarro Ishizawa <a.navarro.ishizawa@gmail.com>
2022-11-30 22:57:18 -0300

    feat: api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::resource('movies', MovieController::class);
+    Route::resource('tags', TagController::class);
 });
